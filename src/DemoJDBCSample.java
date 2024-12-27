90dfc1d90178e85a3df325ee62a10defd123e6c1
juanadls <eleo.dev09@gmail.com>
2024-12-27 14:47:04 -0300

    loading and register on driver done

---
+++ src/DemoJDBCSample.java
@@ -0,0 +1,11 @@
+
+
+
+public class DemoJDBCSample {
+    public static void main(String[] args) throws ClassNotFoundException {
+
+//Load and register:
+        Class.forName("org.postgresql.Driver");
+
+    }
+}
