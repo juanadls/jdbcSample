46b5caad80f92f681f650c624fc9d74e89b6d489
juanadls <eleo.dev09@gmail.com>
2024-12-27 14:55:14 -0300

    connection established

--- src/DemoJDBCSample.java
+++ src/DemoJDBCSample.java
@@ -1,11 +1,16 @@
-
-
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class DemoJDBCSample {
-    public static void main(String[] args) throws ClassNotFoundException {
+    public static void main(String[] args) throws Exception {
 
-//Load and register:
+        //Load and register:
         Class.forName("org.postgresql.Driver");
+        //Connect app to DB
 
+        String url = "jdbc:postgresql://localhost:5432/demo";
+        String userName  = "postgres";
+        String password  = "0000";
+        Connection connection = DriverManager.getConnection(url, userName, password);
     }
 }
