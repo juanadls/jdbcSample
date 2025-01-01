c8c38f4fd32186312f014e23348b1b657c9a4192
juanadls <eleo.dev09@gmail.com>
2025-01-01 17:01:12 -0300

    added properties to handle auth

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import org.postgresql.ds.PGSimpleDataSource;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class Main {
@@ -12,6 +15,17 @@ public class Main {
             props.load(Files.newInputStream(Path.of("demo.properties"),
                     StandardOpenOption.READ));
         } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        var databaseSource = new PGSimpleDataSource();
+        databaseSource.setServerNames(new String[]{props.getProperty("serverName")});
+        databaseSource.setPortNumbers(new int[]{Integer.parseInt(props.getProperty("port"))});
+        databaseSource.setDatabaseName(props.getProperty("databaseName"));
+
+        try (var connection = databaseSource.getConnection(props.getProperty("user"), System.getenv("POSTGRESQL_PASS"))
+        ) {
+            System.out.println("Success");
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
